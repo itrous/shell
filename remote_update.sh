49120e9d5e407d1a435960167bfeefa650534f65
kirill e <itrous@gmail.com>
2024-06-22 22:16:13 +0300

    добавил версию в параметры

--- remote_update.sh
+++ remote_update.sh
@@ -1,3 +1,8 @@
-scp /путь/к/файлу/setup-full-$NEW_VERSION-x86_64.run ubuntu@dev:~/
+#!/bin/bash
+
+# Параметры версий
+NEW_VERSION="8.3.25.1286"
+
+scp ./setup-full-$NEW_VERSION-x86_64.run ubuntu@dev:~/
 scp update_service.sh ubuntu@dev:~/
 ssh ubuntu@dev 'sudo bash ~/update_service.sh true true'

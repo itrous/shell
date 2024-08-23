1bb7a6e0e65208f45f7311e14d266f319104b194
kirill e <itrous@gmail.com>
2024-08-23 22:35:36 +0300

    возможность обновления сразу нескольких серверов

--- remote_update.sh
+++ remote_update.sh
@@ -8,15 +8,15 @@ fi
 
 # Проверяем, передано ли имя сервера
 if [ -z "$2" ]; then
-  echo "Пожалуйста, укажите имя сервера."
+  echo "Пожалуйста, укажите одно или несколько имен серверов через запятую."
   exit 1
 fi
 
 # Получаем путь к установочному файлу из параметра
 installer_path="$1"
 
-# Получаем имя сервера из второго параметра
-server_name="$2"
+# Получаем список серверов из второго параметра
+server_list="$2"
 
 # Проверяем, передан ли параметр -debug
 debug_flag=""
@@ -32,9 +32,19 @@ if [ -z "$NEW_VERSION" ]; then
   exit 1
 fi
 
-# Копируем установочный файл и скрипт на удаленный сервер
-scp "$installer_path" ubuntu@"$server_name":~/
-scp 1c-install.sh ubuntu@"$server_name":~/
+# Разбиваем список серверов на массив
+IFS=',' read -ra SERVERS <<< "$server_list"
 
-# Выполняем скрипт на удаленном сервере, передавая путь к установочному файлу и флаг отладки, если он есть
-ssh ubuntu@"$server_name" "sudo bash ~/1c-install.sh ~/$(basename "$installer_path") $debug_flag"
+# Проходим по каждому серверу и выполняем обновление
+for server_name in "${SERVERS[@]}"; do
+  echo "Обновление сервера: $server_name"
+
+  # Копируем установочный файл и скрипт на удаленный сервер
+  scp "$installer_path" ubuntu@"$server_name":~/
+  scp 1c-install.sh ubuntu@"$server_name":~/
+
+  # Выполняем скрипт на удаленном сервере, передавая путь к установочному файлу и флаг отладки, если он есть
+  ssh ubuntu@"$server_name" "sudo bash ~/1c-install.sh ~/$(basename "$installer_path") $debug_flag"
+
+  echo "Обновление сервера $server_name завершено."
+done

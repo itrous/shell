e09e89e161b908321f4824fbbb3677bcaa59e892
kirill e <itrous@gmail.com>
2024-08-23 21:31:14 +0300

    add new script

--- remote_update.sh
+++ remote_update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Параметры версий
-NEW_VERSION="8.3.25.1286"
+# Получаем путь к установочному файлу из параметра
+installer_path="$1"
 
-scp ./setup-full-$NEW_VERSION-x86_64.run ubuntu@dev:~/
-scp update_service.sh ubuntu@dev:~/
-ssh ubuntu@dev 'sudo bash ~/update_service.sh true true'
+# Извлекаем версию из имени файла
+NEW_VERSION=$(basename "$installer_path" | grep -oP '\d+\.\d+\.\d+\.\d+')
+
+scp $installer_path ubuntu@dev:~/
+scp 1c-install.sh ubuntu@dev:~/
+ssh ubuntu@dev 'sudo bash ~/1c-install.sh ~/installer_path'

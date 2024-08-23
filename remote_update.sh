76aa03259a92d395d52fb8b3246127519b8dc70a
kirill e <itrous@gmail.com>
2024-08-23 21:34:58 +0300

    add server name

--- remote_update.sh
+++ remote_update.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# Проверяем, передан ли параметр
+if [ -z "$1" ]; then
+  echo "Пожалуйста, укажите путь к установочному файлу."
+  exit 1
+fi
+
+# Проверяем, передано ли имя сервера
+if [ -z "$2" ]; then
+  echo "Пожалуйста, укажите имя сервера."
+  exit 1
+fi
+
 # Получаем путь к установочному файлу из параметра
 installer_path="$1"
 
-# Извлекаем версию из имени файла
-NEW_VERSION=$(basename "$installer_path" | grep -oP '\d+\.\d+\.\d+\.\d+')
+# Получаем имя сервера из второго параметра
+server_name="$2"
 
-scp $installer_path ubuntu@dev:~/
-scp 1c-install.sh ubuntu@dev:~/
-ssh ubuntu@dev 'sudo bash ~/1c-install.sh ~/installer_path'
+# Копируем установочный файл и скрипт на удаленный сервер
+scp "$installer_path" ubuntu@"$server_name":~/
+scp 1c-install.sh ubuntu@"$server_name":~/
+
+# Выполняем скрипт на удаленном сервере, передавая путь к установочному файлу
+ssh ubuntu@"$server_name" "sudo bash ~/1c-install.sh ~/$(basename "$installer_path")"
